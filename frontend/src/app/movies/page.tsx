d6ec9197d438b48529d3c85a0d60c18775f42fee
arbisyarifudin@gmail.com <arbisyarifudin@gmail.com>
2024-07-31 02:14:40 +0700

    feat: add pagination and empty state to MoviePage

--- frontend/src/app/movies/page.tsx
+++ frontend/src/app/movies/page.tsx
@@ -1,34 +1,54 @@
 'use client';
-
+import classnames from 'classnames';
 import useIsMobile from '@/hooks/isMobile';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { Button } from '@/components/Button';
 export default function MoviePage() {
-    const movies = [
-        {
-            id: 1,
-            title: 'The Shawshank Redemption',
-            publishingYear: 1994,
-            poster: 'https://picsum.photos/200/400?random=1',
-        },
-        {
-            id: 2,
-            title: 'The Godfather',
-            publishingYear: 1972,
-            poster: 'https://picsum.photos/200/400?random=2',
-        },
-        {
-            id: 3,
-            title: 'The Dark Knight',
-            publishingYear: 2008,
-            poster: 'https://picsum.photos/200/400?random=3',
-        },
-    ]
+    const dummyDatas = [
+        // {
+        //     id: 1,
+        //     title: 'The Shawshank Redemption',
+        //     publishingYear: 1994,
+        //     poster: 'https://picsum.photos/200/400?random=1',
+        // },
+        // {
+        //     id: 2,
+        //     title: 'The Godfather',
+        //     publishingYear: 1972,
+        //     poster: 'https://picsum.photos/200/400?random=2',
+        // },
+        // {
+        //     id: 3,
+        //     title: 'The Dark Knight',
+        //     publishingYear: 2008,
+        //     poster: 'https://picsum.photos/200/400?random=3',
+        // },
+    ];
+
+    const [movies, setMovies] = useState<
+        { id: number; title: string; publishingYear: number; poster: string }[]
+    >([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage] = useState(6);
+
+    useEffect(() => {
+        fetchMovies();
+    }, []);
+
+    const fetchMovies = async () => {
+        setMovies(dummyDatas);
+    };
 
     return (
         <div className="p-5">
             <Header />
-            <MovieList movies={movies} />
+            <MovieList
+                movies={movies}
+                pagination={{ currentPage, itemsPerPage, totalItems: 30 }}
+                handlePageChange={setCurrentPage}
+            />
         </div>
     );
 }
@@ -91,24 +111,136 @@ const Header = () => {
     );
 };
 
-const MovieList = ({ movies }: { movies: any[] }) => {
+const MovieList = ({
+    movies,
+    pagination,
+    handlePageChange,
+}: {
+    movies: {
+        id: number;
+        title: string;
+        publishingYear: number;
+        poster: string;
+    }[];
+    pagination: {
+        currentPage: number;
+        itemsPerPage: number;
+        totalItems: number;
+    };
+    handlePageChange: (pageNumber: number) => void;
+}) => {
     return (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-5">
-            {movies.map((movie, index) => (
-                <Movie key={index} {...movie} />
-            ))}
-        </div>
+        <>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-5">
+                {movies.map((movie, index) => (
+                    <Movie key={index} {...movie} />
+                ))}
+            </div>
+            {
+                movies.length === 0 ? (
+                    <MovieEmpty />
+                ) : (
+                    <Pagination
+                        currentPage={pagination.currentPage}
+                        itemsPerPage={pagination.itemsPerPage}
+                        totalItems={pagination.totalItems}
+                        onPageChange={handlePageChange}
+                    />
+                )
+            }
+        </>
     );
-}
+};
 
-const Movie = ({ id, title, publishingYear, poster }: { id: number, title: string, publishingYear: number, poster: string }) => {
+const Movie = ({
+    id,
+    title,
+    publishingYear,
+    poster,
+}: {
+    id: number;
+    title: string;
+    publishingYear: number;
+    poster: string;
+}) => {
     return (
-        <Link className="bg-card rounded-xl shadow-md cursor-pointer md:p-2" href={`/movies/${id}/edit`}>
-            <img src={poster} alt={title} className="w-full h-[246px] sm:h-[320px] md:h-[400px] object-cover rounded-tl-xl rounded-tr-xl md:rounded-xl" />
+        <Link
+            className="bg-card rounded-xl shadow-md cursor-pointer md:p-2"
+            href={`/movies/${id}/edit`}
+        >
+            <img
+                src={poster}
+                alt={title}
+                className="w-full h-[246px] sm:h-[320px] md:h-[400px] object-cover rounded-tl-xl rounded-tr-xl md:rounded-xl"
+            />
             <div className="p-3">
-                <h3 className="text-base sm:text-[18px] md:text-[20px] font-bold md:font-normal leading-6 mb-4" style={{ minHeight: '50px' }}>{title}</h3>
+                <h3
+                    className="text-base sm:text-[18px] md:text-[20px] font-bold md:font-normal leading-6 mb-4"
+                    style={{ minHeight: '50px' }}
+                >
+                    {title}
+                </h3>
                 <p className="text-sm text-white">{publishingYear}</p>
             </div>
         </Link>
     );
-}
+};
+
+const Pagination = ({
+    currentPage,
+    itemsPerPage,
+    totalItems,
+    onPageChange,
+}: {
+    currentPage: number;
+    itemsPerPage: number;
+    totalItems: number;
+    onPageChange: (pageNumber: number) => void;
+}) => {
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+    const handleClick = (pageNumber: number) => {
+        onPageChange(pageNumber);
+    };
+
+    return (
+        <div className="font-semibold mt-20 flex justify-center">
+            <button
+                className="mr-2"
+                onClick={() => handleClick(currentPage - 1)}
+                disabled={currentPage === 1}
+            >
+                Prev
+            </button>
+            {[...Array(totalPages)].map((_, index) => (
+                <button
+                    key={index}
+                    onClick={() => handleClick(index + 1)}
+                    className={classnames(
+                        currentPage === index + 1 ? 'bg-primary' : 'bg-card',
+                        'mx-1 py-1 px-3 rounded-[4px] transition-colors duration-300'
+                    )}
+                >
+                    {index + 1}
+                </button>
+            ))}
+            <button
+                className="ms-2"
+                onClick={() => handleClick(currentPage + 1)}
+                disabled={currentPage === totalPages}
+            >
+                Next
+            </button>
+        </div>
+    );
+};
+
+const MovieEmpty = () => {
+    const router = useRouter()
+    return (
+        <div className="flex flex-col items-center justify-center mt-[150px] md:mt-[120px]">
+            <h3 className="text-[32px] leading-10 md:text-5xl md:leading-[56px] font-semibold mb-10 text-center">Your movie list is empty</h3>
+            <Button className="mt-5 !text-base w-full sm:w-auto" label="Add a new Movie" onClick={() => router.push('/movies/create')} />
+        </div>
+    );
+};
