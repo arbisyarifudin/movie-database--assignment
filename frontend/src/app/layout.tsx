a2d118a154f7e49e57f1d22be82f643dcb87e14c
arbisyarifudin@gmail.com <arbisyarifudin@gmail.com>
2024-07-30 09:42:53 +0700

    chore: Update layout components

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,12 +16,14 @@ export const metadata: Metadata = {
 export default function RootLayout({
     children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
     return (
         <html lang="en">
             <body className={montserrat.className}>
-                {children}
+                <main className="bg-background">
+                    {children}
+                </main>
                 <Bottom />
             </body>
         </html>
--- frontend/src/components/pages/Bottom.tsx
+++ frontend/src/components/pages/Bottom.tsx
@@ -4,8 +4,8 @@ import Wave from '../Wave';
 
 export function Bottom() {
     return (
-        <div className="fixed bottom-0 left-0 right-0">
+        <footer className="fixed bottom-0 left-0 right-0">
             <Wave />
-        </div>
+        </footer>
     );
 }
