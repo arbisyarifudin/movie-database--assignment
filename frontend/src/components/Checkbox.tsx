1da661570db2d9956840a41aa6f2db9362563420
arbisyarifudin@gmail.com <arbisyarifudin@gmail.com>
2024-07-31 17:38:18 +0700

    chore: Update Checkbox component styling for checked state

--- frontend/src/components/Checkbox.tsx
+++ frontend/src/components/Checkbox.tsx
@@ -23,7 +23,7 @@ export const Checkbox = ({
             <div className="relative flex items-center mr-3">
                 <input
                     type="checkbox"
-                    className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all bg-input before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-primary checked:bg-primary checked:before:bg-primary hover:before:opacity-10"
+                    className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all bg-input before:absolute before:top-2/4 before:left-2/4 before:block before:h-10 before:w-10 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-primary checked:bg-primary checked:before:bg-primary hover:checked:bg-primary hover:before:opacity-10 hover:bg-green-800"
                     onChange={onChange}
                     id={props.id}
                 />
