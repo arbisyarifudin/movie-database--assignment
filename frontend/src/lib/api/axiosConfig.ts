5d0ec49d4433219b9690765eaaf8af739fe7e1de
arbisyarifudin@gmail.com <arbisyarifudin@gmail.com>
2024-08-01 20:51:55 +0700

    update frontend/.env

--- frontend/src/lib/api/axiosConfig.ts
+++ frontend/src/lib/api/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.API_BASE_URL || 'http://localhost:4000',
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:4000',
     headers: {
         'Content-Type': 'application/json',
     },
