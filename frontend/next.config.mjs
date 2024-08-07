6543833e60e8d1d3a93eaf430a72283a3c30c51c
arbisyarifudin@gmail.com <arbisyarifudin@gmail.com>
2024-08-07 19:39:39 +0700

    chore: Update Dockerfile.local and docker-compose.local.yml to enable hot reloading for next.js on windows

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost', '43.218.241.82'],
+        domains: ["localhost", "43.218.241.82"],
     },
+    // webpack: (config, context) => {
+    //     config.watchOptions = {
+    //         poll: 1000,
+    //         aggregateTimeout: 300,
+    //     };
+    //     return config;
+    // },
 };
 
 export default nextConfig;
